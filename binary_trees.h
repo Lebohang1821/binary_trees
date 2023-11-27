d7920c87707ec5d3244c361f3876987955468091
Motekema <motekema@gmail.com>
2023-11-27 03:03:22 -0800

    My commit

--- binary_trees.h
+++ binary_trees.h
@@ -9,8 +9,21 @@ typedef struct binary_tree_s {
     struct binary_tree_s *right;
 } binary_tree_t;
 
-/* Function prototype */
+/* Data structure for an even binary tree node */
+typedef struct binary_tree_even_s {
+    int n;
+    struct binary_tree_even_s *parent;
+    struct binary_tree_even_s *left;
+    struct binary_tree_even_s *right;
+} binary_tree_even_t;
+
+/* Function prototypes for regular binary tree operations */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/* Function prototypes for even binary tree operations */
+binary_tree_even_t *binary_tree_even_node(binary_tree_even_t *parent, int value);
+binary_tree_even_t *binary_tree_even_insert_left(binary_tree_even_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
 
