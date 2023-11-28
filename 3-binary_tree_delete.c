2ae45b612399bcaeb97fa3dba992c6c0f41ad9a1
Lebohang1821 <millboylebohang@gmail.com>
2023-11-28 12:27:42 -0800

    task-3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_delete - It del binary tree
- * @tree: Pointer to root node of tree to delete.
+ * @my_tree: Pointer to root node of tree to delete.
  */
 void binary_tree_delete(binary_tree_t *my_tree)
 {
