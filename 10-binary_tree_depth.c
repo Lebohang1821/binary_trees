a0f79aa4778908c87a6c7a03cd421aecd6a632a4
Lebohang1821 <millboylebohang@gmail.com>
2023-11-28 12:33:11 -0800

    task-10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,11 +2,11 @@
 
 /**
  * binary_tree_depth - It measures depth of node in binary my_tree
- * @my_tree: Pointer to node to measure depth
+ * @_tree: Pointer to node to measure depth
  *
  * Return: If my_tree is NULL, your function must return 0, else return depth
  */
-size_t binary_tree_depth(const binary_tree_t *my_tree)
+size_t binary_tree_depth(const binary_tree_t *_tree)
 {
-	return ((my_tree && my_tree->parent) ? 1 + binary_tree_depth(my_tree->parent) : 0);
+	return ((_tree && _tree->parent) ? 1 + binary_tree_depth(_tree->parent) : 0);
 }
